6ff5ef65f19c86ad3651534046c3fed914cec03e
Jesse Adamson Paroz <jesse.paroz@gmail.com>
2025-08-26 21:49:33 +1000

    Enable dark/light mode, and tidy up some CSS

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,29 @@ import "./style.css";
 
 import { Grid } from "./grid.ts";
 
+// Theme toggle
+{
+  const root = document.querySelector("html");
+  let newTheme = "light";
+
+  const localTheme = localStorage.getItem("theme");
+  if (localTheme === null) {
+    const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+    if (systemSettingDark.matches) {
+      newTheme = "dark";
+    }
+  } else {
+    newTheme = localTheme;
+  }
+  root.setAttribute("data-theme", newTheme);
+  document.getElementById("theme-toggle").addEventListener("click", () => {
+    const oldTheme = root.getAttribute("data-theme");
+    const newTheme = oldTheme === "dark" ? "light" : "dark";
+    root.setAttribute("data-theme", newTheme);
+
+    // update in local storage
+    localStorage.setItem("theme", newTheme);
+  });
+}
+
 let grid = new Grid(9, 9);
